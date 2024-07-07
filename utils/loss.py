70037da206db6682111c9fca1ac0700171329608
zhengli <1429053840@qq.com>
2024-07-07 11:14:03 +0800

    add comments

--- utils/loss.py
+++ utils/loss.py
@@ -4,5 +4,5 @@ import torch
 def cross_entropy_loss_and_accuracy(prediction, target):
     cross_entropy_loss = torch.nn.CrossEntropyLoss()
     loss = cross_entropy_loss(prediction, target)
-    accuracy = (prediction.argmax(1) == target).float().mean()
+    accuracy = (prediction.argmax(1) == target).float().mean() # 预测值中数值最大的并且预测的值和目标值相等，转换成float数值，然后求平均
     return loss, accuracy
